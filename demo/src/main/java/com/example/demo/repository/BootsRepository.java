e1e893d692ed10ffc63411255abcf2493d029b06
Razielruss <semkedennis123@gmx.de>
2025-10-25 14:59:31 +0200

    ReadMe angepasst

--- demo/src/main/java/com/example/demo/repository/BootsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-import org.springframework.data.repository.Repository;
-import com.example.demo.model.*;
-
-import com.example.demo.*;
-
-public interface BootsRepository extends Repository<Boots, Long>  {
-
-}
